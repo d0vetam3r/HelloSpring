f59145f8ffcd5a892b9ea9e032dda7c5a43cfda0
d0vetam3r <d0vetam3r@gmail.com>
2023-01-12 14:53:52 +0900

    controller의 동작개념과 templetes 하위 html파일의 연결 확인

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,23 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    //get으로 hello라는 데이터가 넘어왔을때(@GetMapping("hello"))
+    //hello1.html viewpage로 연결한다(return값에 해당하는 html페이지)
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data", "hello!!");
+        return "hello1.html";
+    }
+
+    @GetMapping("login")
+    public String login(Model model){
+        model.addAttribute("data", "login완료");
+        return "/LoginPage/login.html";
+    }
+}
